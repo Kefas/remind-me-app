b04307080e295ea4105a3cec36c170693e03033d
Agnieszka Szczurek <agnieszkaszczurek93@gmail.com>
2015-10-25 19:56:15 +0100

    Initial commit.

---
+++ remind-me-app/remind-me-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  remind-me-app
+//
+//  Created by Agnieszka Szczurek on 25.10.2015.
+//  Copyright © 2015 AgnieszkaSzczurek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ remind-me-app/remind-me-appTests/remind_me_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  remind_me_appTests.swift
+//  remind-me-appTests
+//
+//  Created by Agnieszka Szczurek on 25.10.2015.
+//  Copyright © 2015 AgnieszkaSzczurek. All rights reserved.
+//
+
+import XCTest
+@testable import remind_me_app
+
+class remind_me_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
