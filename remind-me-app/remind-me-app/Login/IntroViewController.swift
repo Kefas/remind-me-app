c4c3b180e094b362123ad632243ef2f3275120bd
Agnieszka Szczurek <agnieszkaszczurek93@gmail.com>
2015-10-26 23:59:05 +0100

    Added simply intro animation.

---
+++ remind-me-app/remind-me-app/Login/IntroViewController.swift
@@ -0,0 +1,45 @@
+//
+//  IntroViewController.swift
+//  remind-me-app
+//
+//  Created by Agnieszka Szczurek on 26.10.2015.
+//  Copyright © 2015 AgnieszkaSzczurek. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var verticalCenterConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageWidthConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        logoImageView.alpha = 0
+        animateLogo()
+    }
+    
+    func animateLogo() {
+       
+        UIView.animateWithDuration(1.5, animations: { () -> Void in
+                self.logoImageView.alpha = 1
+            }) { (Bool) -> Void in
+                self.moveImageToTop()
+        }
+    }
+    
+    func moveImageToTop() {
+        self.view.layoutIfNeeded()
+        self.verticalCenterConstraint.active = false
+        self.imageHeightConstraint.constant = 150
+        self.imageWidthConstraint.constant = 150
+
+        UIView.animateWithDuration(2.0, delay: 0.0, options: .CurveEaseIn, animations: { () -> Void in
+            self.view.layoutIfNeeded()
+            }) { (Bool) -> Void in
+                
+        }
+    }
+}
